4ec529c9ffe55f65ef012ffcb24f1dc6a2d3a718
Anil Anar <anilanar@hotmail.com>
2018-05-14 00:49:57 +0200

    globalize common button styles

--- client/src/components/Authentication/Logout.jsx
+++ client/src/components/Authentication/Logout.jsx
@@ -3,7 +3,9 @@ import { connect } from 'react-redux';
 import { logout } from '../../services/authentication/actions';
 
 const LogoutView = ({ logout }) => (
-  <button onClick={logout}>Logout</button>
+  <button onClick={logout}>
+    Logout
+  </button>
 );
 
 export const Logout = connect(undefined, { logout })(LogoutView);
